9b1596da2e080ac39101e9dce436685476493f2c
Andreas Limi <andreaslimidev@gmail.com>
2022-01-11 10:44:00 +0100

    📦 NEW: refresh and fetchUser util

--- services/auth.ts
+++ services/auth.ts
@@ -1,5 +1,15 @@
 /* will request the backend to remove auth cookies from clienty */
-export function logout() {
+export async function logout() {
   const url = `${process.env.API_URL}/auth/logout`;
-  fetch(url, { method: "GET", credentials: "include" });
+  return fetch(url, { method: "GET", credentials: "include" });
+}
+
+export async function refreshAccessToken() {
+  const url = `${process.env.API_URL}/auth/refresh`;
+  return fetch(url, { method: "GET", credentials: "include" });
+}
+
+export async function fetchUser() {
+  const url = `${process.env.API_URL}/auth/user`;
+  return fetch(url, { method: "GET", credentials: "include" });
 }
