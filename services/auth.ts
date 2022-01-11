3e5b5d745c61352364c200eabcd4707aeff88b81
Andreas Limi <andreaslimidev@gmail.com>
2022-01-11 12:00:40 +0100

    📖 DOC: auth.ts

--- services/auth.ts
+++ services/auth.ts
@@ -4,11 +4,13 @@ export async function logout() {
   return fetch(url, { method: "GET", credentials: "include" });
 }
 
+/* will refresh access token by using a refresh token */
 export async function refreshAccessToken() {
   const url = `${process.env.API_URL}/auth/refresh`;
   return fetch(url, { method: "GET", credentials: "include" });
 }
 
+/* will fetch user data (access token is required) */
 export async function fetchUser() {
   const url = `${process.env.API_URL}/auth/user`;
   return fetch(url, { method: "GET", credentials: "include" });
