1cff98b3700ec12bfc9205bd4058653248627bb7
Andreas Limi <andreaslimidev@gmail.com>
2022-01-12 11:43:25 +0100

    📦 NEW: config for prod

--- next.config.js
+++ next.config.js
@@ -1,10 +1,18 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-  sassOptions: {
-    prependData: `@import "variables"; @import "mixins";`,
-},
-  env: {
-    API_URL: "http://localhost:3000"
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
+
+module.exports = (phase) => {
+  /** @type {import('next').NextConfig} */
+
+  const nextConfig = {
+    reactStrictMode: true,
+    sassOptions: {
+      prependData: `@import "variables"; @import "mixins";`,
+  },
+    env: {
+      API_URL: phase === PHASE_DEVELOPMENT_SERVER ? "http://localhost:3000" : "https://peoply.azurewebsites.net"
+    }
   }
+
+  return nextConfig;
+
 }
