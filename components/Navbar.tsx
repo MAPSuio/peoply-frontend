9aba4a2d76ac87da021c1890264187cce7752c1f
Andreas Limi <andreaslimidev@gmail.com>
2022-01-14 13:22:41 +0100

    📦 NEW: scroll hook

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,18 +1,46 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import styles from "../styles/Navbar.module.scss";
+import AddIcon from "./icons/AddIcon";
 import HomeIcon from "./icons/HomeIcon";
 
 export default function Navbar() {
+  const [hidden, setHidden] = useState(false);
+  const [scrollPos, setScrollPos] = useState(-1);
+
   const router = useRouter();
+
+  /* hooks onto window scroll event to determine if
+     navbar should be hidden */
+  useEffect(() => {
+    if (scrollPos < 0) {
+      setScrollPos(window.pageYOffset);
+    } else {
+      const onscroll = function () {
+        const currentScrollPos = window.pageYOffset;
+        if (scrollPos > currentScrollPos) {
+          setHidden(false);
+        } else {
+          setHidden(true);
+        }
+        setScrollPos(currentScrollPos);
+      };
+      window.addEventListener("scroll", onscroll);
+
+      return () => {
+        window.removeEventListener("scroll", onscroll);
+      };
+    }
+  }, [scrollPos]);
 
   /* matches given string on path (after host url -> /path) */
   const isActive = (href: string) => router.pathname === href;
 
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.container}>
-        <div className={styles.item}>
+    <div className={`${styles.wrapper} ${hidden ? styles.hidden : ""}`}>
+      <div tabIndex={1} className={styles.container}>
+        <div className={`${styles.item} ${isActive("/") ? styles.active : ""}`}>
           <Link href="/" passHref>
             <a>
               <HomeIcon />
@@ -20,35 +48,22 @@ export default function Navbar() {
             </a>
           </Link>
         </div>
-        <div className={styles.item}>
+        <div className={`${isActive("/create") ? styles.active : ""}`}>
+          <Link href="/test" passHref>
+            <a>
+              <AddIcon classNames={styles.button} />
+            </a>
+          </Link>
+        </div>
+        <div
+          className={`${styles.item} ${
+            isActive("/login") ? styles.active : ""
+          }`}
+        >
           <Link href="/login" passHref>
             <a>
               <HomeIcon />
               {isActive("/login") ? (
-                <span className={styles.underline}></span>
-              ) : (
-                ""
-              )}
-            </a>
-          </Link>
-        </div>
-        <div className={styles.item}>
-          <Link href="/events" passHref>
-            <a>
-              <HomeIcon />
-              {isActive("/something") ? (
-                <span className={styles.underline}></span>
-              ) : (
-                ""
-              )}
-            </a>
-          </Link>
-        </div>
-        <div className={styles.item}>
-          <Link href="/events" passHref>
-            <a>
-              <HomeIcon />
-              {isActive("/other") ? (
                 <span className={styles.underline}></span>
               ) : (
                 ""
