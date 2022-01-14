7ec69ceabf01bd7e05c4c1bbff4e018b8e49681e
Andreas Limi <andreaslimidev@gmail.com>
2022-01-14 13:23:27 +0100

    📦 NEW: AddIcon

---
+++ components/icons/AddIcon.tsx
@@ -0,0 +1,14 @@
+import styles from "../../styles/AddIcon.module.scss";
+
+interface AddIconProps {
+  classNames?: string;
+}
+
+export default function AddIcon({ classNames }: AddIconProps) {
+  return (
+    <div className={`${styles.container} ${classNames}`}>
+      <span />
+      <span />
+    </div>
+  );
+}
