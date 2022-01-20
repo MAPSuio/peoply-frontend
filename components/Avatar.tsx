b77d991ba7b573f6efc48c3f25657d331af1d5a5
Andreas Limi <andreaslimidev@gmail.com>
2022-01-20 08:43:08 +0100

    👌 IMPROVE: avatar size prop + removed indicator

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -3,16 +3,29 @@ import styles from "../styles/Avatar.module.scss";
 
 interface UserProps {
   user: User;
+  size?: "small" | "medium" | "large";
 }
 
-export default function Avatar({ user }: UserProps) {
+export default function Avatar({ user, size }: UserProps) {
+  const getSizeStyling = () => {
+    switch (size) {
+      case "small":
+        return styles.small;
+      case "medium":
+        return styles.medium;
+      case "large":
+        return styles.large;
+      default:
+        return styles.medium;
+    }
+  };
   return (
     <div>
-      <div className={styles.avatar}>
+      <div className={`${styles.avatar} ${getSizeStyling()}`}>
         <span className={styles.name}>
           {`${user.first_name.charAt(0)}${user.last_name.charAt(0)}`}
         </span>
-        <span className={styles.indicator}></span>
+        {/* <span className={styles.indicator}></span> */}
       </div>
     </div>
   );
