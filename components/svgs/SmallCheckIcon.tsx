f36b9f2d2da83f32dea4a959401935b81442f68b
Maximilian von Stephanides <maximilianvons@gmail.com>
2022-01-20 14:05:48 +0100

    👌 IMPROVE: Remove optional h/w from icon

--- components/svgs/SmallCheckIcon.tsx
+++ components/svgs/SmallCheckIcon.tsx
@@ -1,13 +1,8 @@
-interface SmallCheckIconProps {
-  width?: number;
-  height?: number;
-}
-
-export default function SmallCheckIcon({ width, height }: SmallCheckIconProps) {
+export default function SmallCheckIcon() {
   return (
     <svg
-      width={width ? width : "14"}
-      height={height ? height : "14"}
+      width="14"
+      height="14"
       viewBox="0 0 14 14"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
