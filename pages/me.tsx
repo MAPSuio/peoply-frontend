893e5d29f33cf02602b0048ed14216a71f522975
Andreas Limi <andreaslimidev@gmail.com>
2022-01-20 10:53:50 +0100

    📦 NEW: profile comp styled

---
+++ pages/me.tsx
@@ -0,0 +1,44 @@
+import { NextPage } from "next";
+import { useRouter } from "next/router";
+import Avatar from "../components/Avatar";
+import BackButton from "../components/BackButton";
+import useUser from "../hooks/useUser";
+import styles from "../styles/me.module.scss";
+
+const Me: NextPage = () => {
+  const { user, loading } = useUser();
+
+  const router = useRouter();
+
+  if (loading) {
+    return <></>;
+  }
+
+  if (!loading && !user) {
+    router.push("/login");
+  }
+
+  if (!loading && user) {
+    return (
+      <div className={styles.container}>
+        <BackButton onClick={() => router.push("/", {})} />
+        <div className={styles.profile}>
+          <Avatar user={user} size="large" />
+          <h1
+            className={styles.name}
+          >{`${user.first_name} ${user.last_name}`}</h1>
+          <p className={styles.location}>Oslo, NO</p>
+          <p className={styles.description}>
+            Descriptions is here can you see this shit or what Descriptions is
+            here can you see this shit or what Descriptions is here can you see
+            this shit
+          </p>
+        </div>
+      </div>
+    );
+  }
+
+  return <></>;
+};
+
+export default Me;
