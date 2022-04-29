5582986bc0b7266f7e2790452072652a1e32a627
Christoffer Bjelke <56649743+chribjel@users.noreply.github.com>
2022-04-29 19:09:36 +0200

    remove pwa functionality (#260)

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
-const withPWA = require('next-pwa');
+// const withPWA = require('next-pwa');
 
-module.exports = withPWA({
+module.exports = /* withPWA( */{
   /** @type {import('next').NextConfig} */
 
   i18n: {
@@ -26,9 +26,10 @@ module.exports = withPWA({
   },
 
   // generate manifest using https://www.simicart.com/
-  pwa: {
-    dest: 'public',
-    disable: process.env.NODE_ENV === 'development',
-    register: true,
-  }
-});
+  // pwa: {
+    // dest: 'public',
+    // disable: process.env.NODE_ENV === 'development',
+    // disable: true,
+    // register: true,
+  // }
+}/* ) */;
