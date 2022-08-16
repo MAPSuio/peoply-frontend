71a85b2015e2fcb73d223ca381bb8f6331ff1216
Christoffer Bjelke <56649743+chribjel@users.noreply.github.com>
2022-08-16 20:24:06 +0200

    more capability in backbutton (#304)

--- hooks/useBack.tsx
+++ hooks/useBack.tsx
@@ -1,10 +1,12 @@
 import { useRouter } from "next/router";
 
-const useBack = () => {
+const useBack = (fallbackUrl?: string) => {
   const router = useRouter();
 
   return () => {
-    router.back();
+    if (history.length > 2) router.back();
+    else if (fallbackUrl) router.push(fallbackUrl);
+    else router.push("/");
   };
 };
 
