449a38e174fb127464f4aa6b3fc6c76785bf17a3
Christoffer Bjelke <56649743+chribjel@users.noreply.github.com>
2022-08-29 20:55:25 +0200

    add tracking to the app (#353)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,26 +5,30 @@ import { UserProvider } from "../hooks/useUser";
 import { SnackbarProvider } from "../hooks/useSnack";
 import Head from "next/head";
 import { NotificationsProvider } from "../hooks/useNotifications";
+import { AppInsightsContext } from "@microsoft/applicationinsights-react-js";
+import { reactPlugin } from "../AzureApplicationInsight/AppInsight";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <UserProvider>
-      <SnackbarProvider>
-        <NotificationsProvider>
-          <Head>
-            <meta
-              name="viewport"
-              content="width=device-width, initial-scale=1"
-            />
-          </Head>
-          <div className={styles.wrapper}>
-            <div className={styles.container}>
-              <Component {...pageProps} />
+    <AppInsightsContext.Provider value={reactPlugin}>
+      <UserProvider>
+        <SnackbarProvider>
+          <NotificationsProvider>
+            <Head>
+              <meta
+                name="viewport"
+                content="width=device-width, initial-scale=1"
+              />
+            </Head>
+            <div className={styles.wrapper}>
+              <div className={styles.container}>
+                <Component {...pageProps} />
+              </div>
             </div>
-          </div>
-        </NotificationsProvider>
-      </SnackbarProvider>
-    </UserProvider>
+          </NotificationsProvider>
+        </SnackbarProvider>
+      </UserProvider>
+    </AppInsightsContext.Provider>
   );
 }
 
