1ad495d6e866a469d7f84ea8512ca1675294faa9
Christoffer Bjelke <christoffer.bjelke@outlook.com>
2023-03-23 11:59:17 +0100

    Remove Azure Application Insights and add Vercel Analytics to the project in _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,32 +6,35 @@ import { SnackbarProvider } from "../hooks/useSnack";
 import Head from "next/head";
 import { NotificationsProvider } from "../hooks/useNotifications";
 import { ThemeProvider } from "next-theme";
-// import { AppInsightsContext } from "@microsoft/applicationinsights-react-js";
-// import { reactPlugin } from "../AzureApplicationInsight/AppInsight";
+import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    // <AppInsightsContext.Provider value={reactPlugin}>
-    <UserProvider>
-      <SnackbarProvider>
-        <NotificationsProvider>
-          <ThemeProvider attribute="class" themes={["light", "dark", "night"]}>
-            <Head>
-              <meta
-                name="viewport"
-                content="width=device-width, initial-scale=1"
-              />
-            </Head>
-            <div className={styles.wrapper}>
-              <div className={styles.container}>
-                <Component {...pageProps} />
+    <>
+      <UserProvider>
+        <SnackbarProvider>
+          <NotificationsProvider>
+            <ThemeProvider
+              attribute="class"
+              themes={["light", "dark", "night"]}
+            >
+              <Head>
+                <meta
+                  name="viewport"
+                  content="width=device-width, initial-scale=1"
+                />
+              </Head>
+              <div className={styles.wrapper}>
+                <div className={styles.container}>
+                  <Component {...pageProps} />
+                </div>
               </div>
-            </div>
-          </ThemeProvider>
-        </NotificationsProvider>
-      </SnackbarProvider>
-    </UserProvider>
-    // </AppInsightsContext.Provider>
+            </ThemeProvider>
+          </NotificationsProvider>
+        </SnackbarProvider>
+      </UserProvider>
+      <Analytics />
+    </>
   );
 }
 
