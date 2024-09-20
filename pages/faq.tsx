41c8156445d8000c94b122a2dd44fa38a1be0d43
Martiwj <Martin.wich.jorg@gmail.com>
2024-09-20 16:01:55 +0200

    Updated contact information on FAQ page

--- pages/faq.tsx
+++ pages/faq.tsx
@@ -200,13 +200,15 @@ const FAQ = ({ baseUrl }: FAQProps) => {
             </div>
             <div className={styles.section}>
               <span className={styles.divider} />
-              <p className={styles.infoText}>Decidable AS</p>
-              <p className={styles.infoText}>Orgnr: 927 524 627</p>
+              <p className={styles.infoText}>MAPS</p>
+              <p className={styles.infoText}>Orgnr: 995 251 884</p>
               <p className={styles.infoText}>
-                Adresse: Hovseterveien 52 A 0702, 0768 Oslo
+                Adresse: Postboks 1080 Blindern, 0316 Oslo
               </p>
-              <p className={styles.infoText}>Tlfnummer: 993 67 518</p>
-              <p className={styles.infoText}>Email: post@decidable.no</p>
+              <p className={styles.infoText}>Tlfnummer: 904 76 675</p>
+              <p className={styles.infoText}>
+                Email: maps-kontakt@studorg.uio.no
+              </p>
             </div>
           </div>
         </div>
