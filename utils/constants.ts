c28dab0f362d622c625ad0d7b0ec1178d1dc340d
vuhger <victor.uhnger@gmail.com>
2026-07-26 01:37:47 +0200

    feat(header): link to source code from the front page

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,3 +1,6 @@
+/* Peoply is open source; the header links here so visitors can find the code. */
+export const sourceCodeUrl = "https://github.com/MAPSuio/peoply-frontend";
+
 export const eventTitleMinLength = 3;
 export const eventTitleMaxLength = 100;
 
